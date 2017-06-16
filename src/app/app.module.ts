2bfa5341065f06e9d3303f36bd3d42607ce6cab8
Speros Misirlakis <smisirlakis@gmail.com>
2017-06-16 15:32:31 -0700

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { QuoteCreateComponent } from './quote-create/quote-create.component';
+import { QuoteListComponent } from './quote-list/quote-list.component';
+import { OrderbyPipe } from './orderby.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuoteCreateComponent,
+    QuoteListComponent,
+    OrderbyPipe
   ],
   imports: [
     BrowserModule,
